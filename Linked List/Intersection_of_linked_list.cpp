e60166e69b642841ee87cfed3f11a3981152e1bb
Vaibhav Mittal <76803208+VaibhavMittal20@users.noreply.github.com>
2022-10-25 18:27:05 +0530

    Create Intersection_of_linked_list.cpp

---
+++ Linked List/Intersection_of_linked_list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* start1=headA,*start2=headB;
+        int count=0;
+        
+        while(start1!=start2 && count<2)
+        {
+            if(start1->next!=NULL)
+                start1=start1->next;
+            else
+            {
+                start1=headB;
+                count++;
+            }
+            
+            if(start2->next!=NULL)
+                start2=start2->next;
+            else
+                start2=headA;
+        }
+        if(count==2)
+            return NULL;
+        return start1;
+    }
+};
